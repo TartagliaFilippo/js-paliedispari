3892e0a728b1731bdf9591dacb055e5ea61f9414
filippo199600 <pippotartaglia@gmail.com>
2023-07-27 15:52:35 +0200

    Feat even or odd

--- pari e dispari/js/main.js
+++ pari e dispari/js/main.js
@@ -1,11 +1,19 @@
 // chiedo le informazioni all'utente
-// const evenOrOdd = prompt("pari o dispari?");
-// const userNumber = parseInt(prompt("scrivi un numero da 1 a 5"));
+const evenOrOdd = prompt("pari o dispari?");
+const userNumber = parseInt(prompt("scrivi un numero da 1 a 5"));
+
+// stabilisco cos'è pari e cosa è dispari
+let even;
+
+// se l'utente sceglie pari il computer prende dispari
+if (evenOrOdd.length == 4) {
+  even = true;
+} else {
+  even = false;
+}
+console.log(even);
 
 // genero un numero random tra 1 e 5
-const min = 1;
-const max = 5;
-
 function getRandomNumber(min, max) {
   const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
   return randomNumber;
