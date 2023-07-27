bed99645044f16e55a19ab21aaa017a8184b7884
filippo199600 <pippotartaglia@gmail.com>
2023-07-27 17:07:36 +0200

    js sum evenCheck , oddCheck

--- pari e dispari/js/main.js
+++ pari e dispari/js/main.js
@@ -1,29 +1,60 @@
 // chiedo le informazioni all'utente
-// const evenOrOdd = prompt("pari o dispari?");
-// const userNumber = parseInt(prompt("scrivi un numero da 1 a 5"));
+const userEvenOrOdd = prompt("pari o dispari?");
+console.log("l'utente ha scelto: " + userEvenOrOdd);
+const userNumber = parseInt(prompt("scrivi un numero da 1 a 5"));
+console.log("il numero dell'utente è: " + userNumber);
+
+// variabile pari toggle
+let even;
 
 // variabile somma
 let sum = 0;
 
-// stabilisco cos'è pari e cosa è dispari
-let even;
+// variabile del risultato finale
+let evenOrOdd;
+
+// costanti foglio index
+// const userChoiceEl = document.getElementById("user-choice");
+// const userNumberEl = document.getElementById("user-number");
+// const computerNumberEl = document.getElementById("computer-number");
+// const resultEl = document.getElementById("result");
 
 // se l'utente sceglie pari il computer prende dispari
-if (evenOrOdd.length == 4) {
+if (userEvenOrOdd.length == 4) {
   even = true;
 } else {
   even = false;
 }
-console.log(even);
 
-// genero un numero random tra 1 e 5
+// ! funzione numero random tra 1 e 5
 function getRandomNumber(min, max) {
   const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
   return randomNumber;
 }
 
+// uso la funzione per generare il numero del computer
 const computerNumber = getRandomNumber(1, 5);
-console.log(computerNumber);
+console.log("numero del computer è: " + computerNumber);
 
 // sommo i numeri
 sum = userNumber + computerNumber;
+console.log("la somma dei numeri è: " + sum);
+
+// stabilisco cos'è pari e cosa è dispari
+const evenCheck = "pari";
+const oddCheck = "dispari";
+
+// stabilisco se la somma è pari o dispari
+if (sum % 2 == 0) {
+  evenOrOdd = evenCheck;
+} else {
+  evenOrOdd = oddCheck;
+}
+console.log("il risultato delle operazioni è: " + evenOrOdd);
+
+// stampo i valori sul foglio per sistemare il caos in testa
+
+// userEvenOrOdd.innerHTML = userChoiceEl;
+// userNumber.innerHTML = userNumberEl;
+// computerNumber.innerHTML = computerNumberEl;
+// evenOrOdd.innerHTML = resultEl;
